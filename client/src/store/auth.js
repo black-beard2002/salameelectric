c94e5e052710f45360fcbec2a94621a5ae414a60
Ahmad Salemeh <128619518+black-beard2002@users.noreply.github.com>
2025-01-06 03:37:21 +0200

    Updated api in auth.js

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -19,7 +19,7 @@ export const useAuthStore = create(
           };
         }
         try {
-          const response = await fetch("http://localhost:5000/api/login", {
+          const response = await fetch("https://salameelectric.onrender.com/api/login", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
