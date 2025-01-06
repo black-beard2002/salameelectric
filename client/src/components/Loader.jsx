83e6da91189b1b5f50837b47271555f7942b5bc3
Ahmad Salemeh <128619518+black-beard2002@users.noreply.github.com>
2025-01-07 01:50:23 +0200

    Update Loader.jsx

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="flex-col gap-4 w-full flex items-center justify-center">
+    <div className="flex-col gap-4 w-full flex items-center justify-center mx-auto">
       <div className="w-7 h-7 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
         <div className="w-5 h-5 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
       </div>
