5131d1360c69eacd43ac9577f9412870e9c0eada
Ahmad Salemeh <128619518+black-beard2002@users.noreply.github.com>
2025-01-07 17:10:29 +0200

    Update auth.js

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -20,7 +20,7 @@ export const useAuthStore = create(
         }
 
         try {
-          const response = await fetch(`${process.env.HOST_URL}/api/login`, {
+          const response = await fetch(`${import.meta.env.VITE_HOST_URL}/api/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password }),
