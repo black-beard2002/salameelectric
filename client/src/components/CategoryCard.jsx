e05e38e78d6225e2e5125826f0dd57b97d3bf238
Ahmad Salemeh <128619518+black-beard2002@users.noreply.github.com>
2025-01-10 05:18:10 +0200

    Update CategoryCard.jsx

--- client/src/components/CategoryCard.jsx
+++ client/src/components/CategoryCard.jsx
@@ -8,7 +8,7 @@ const CategoryCard = ({
   return (
     <div
       onClick={onClick}
-      className="relative w-[22%]  aspect-square h-fit bg-white dark:bg-zinc-800 rounded-xl shadow-sm hover:shadow-md transition-all duration-300 cursor-pointer group border border-gray-100 dark:border-zinc-700"
+      className="relative w-[22%] h-[5.8rem] xs:aspect-square h-fit bg-white dark:bg-zinc-800 rounded-xl shadow-sm hover:shadow-md transition-all duration-300 cursor-pointer group border border-gray-100 dark:border-zinc-700"
     >
       {newFlag && (
         <div className="absolute top-0 rounded-tr-xl rounded-bl-xl right-0 bg-green-500 text-white px-3 py-1 text-xs font-medium z-10">
