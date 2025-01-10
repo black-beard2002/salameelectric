2b5602e8aec169388cac77d460d2675f9d741b79
Ahmad Salemeh <128619518+black-beard2002@users.noreply.github.com>
2025-01-10 02:32:47 +0200

    Update SideNav.jsx

--- client/src/layouts/SideNav.jsx
+++ client/src/layouts/SideNav.jsx
@@ -9,52 +9,56 @@ import {
   faTableCellsLarge,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
+import { faReadme, faSalesforce } from "@fortawesome/free-brands-svg-icons";
 import logo from "../assets/logo.svg";
 import { useAuthStore } from "../store/auth";
-import { faReadme, faSalesforce } from "@fortawesome/free-brands-svg-icons";
 
-function SideNav() {
-
+function SideNav({
+  isHovered,
+  setIsHovered,
+  isMobile,
+  setIsSidebarOpen,
+  isSidebarOpen,
+}) {
   const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const { isAuthenticated, user, logout } = useAuthStore();
   const location = "https://maps.app.goo.gl/Q11gmmDWxdkkQtKx5";
-
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
 
   const handleLogout = () => {
     logout();
   };
-  // Ref for the sidebar container
+
   const sidebarRef = useRef(null);
-  // Handle clicks outside the sidebar
+
   useEffect(() => {
     const handleClickOutside = (event) => {
-      // Only handle outside clicks on mobile screens
-      if (window.innerWidth < 640) {
-        // Tailwind's sm breakpoint is 640px
-        // Check if sidebar is open and click is outside sidebar
-        if (
-          isSidebarOpen &&
-          sidebarRef.current &&
-          !sidebarRef.current.contains(event.target)
-        ) {
-          setIsSidebarOpen(false);
-        }
+      if (
+        isMobile &&
+        isSidebarOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target)
+      ) {
+        setIsSidebarOpen(false);
       }
     };
 
-    // Add event listener
     document.addEventListener("mousedown", handleClickOutside);
-
-    // Cleanup event listener
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [isSidebarOpen]);
+  }, [isSidebarOpen, isMobile, setIsSidebarOpen]);
+
+  const handleMouseEnter = () => {
+    if (!isMobile) {
+      setIsHovered(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (!isMobile) {
+      setIsHovered(false);
+    }
+  };
 
   return (
     <>
@@ -63,11 +67,7 @@ function SideNav() {
           <div className="flex items-center justify-between">
             <div className="flex items-center justify-start rtl:justify-end">
               <button
-                onClick={toggleSidebar}
-                data-drawer-target="logo-sidebar"
-                data-drawer-toggle="logo-sidebar"
-                aria-controls="logo-sidebar"
-                type="button"
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
                 className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               >
                 <span className="sr-only">Open sidebar</span>
@@ -79,88 +79,55 @@ function SideNav() {
               <a href={location} className="flex ms-2 md:me-24">
                 <img
                   src={logo}
-                  className="h-10 w-10 me-3"
-                  alt="FlowBite Logo"
+                  className="aspect-square w-6 me-3 xs:w-10"
+                  alt="Logo"
                 />
-                <span className="self-center text-xl font-semibold font-sans sm:text-2xl whitespace-nowrap text-[#4c5c68] dark:text-white">
+                <span className="self-center text-[0.95rem] xs:text-xl font-semibold font-sans sm:text-2xl whitespace-nowrap text-[#4c5c68] dark:text-white">
                   <FontAwesomeIcon
                     icon={faBoltLightning}
-                    className="w-5 h-5 "
+                    className="w-3 aspect-square xs:w-5"
                   />
                   Salame Electric
                 </span>
               </a>
             </div>
-            <div className="flex items-center flex-row gap-1">
-              <div>
+            <div className="flex items-center flex-row gap-2">
+              <div className="mt-2">
                 <ThemeToggle />
               </div>
               {isAuthenticated && (
-                <div className="flex items-center ms-3">
-                  <div>
-                    <button
-                      type="button"
-                      className="flex text-sm dark:bg-gray-800 bg-[#1985a1] p-2 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                      aria-expanded={isProfileDropdownOpen}
-                      onClick={() =>
-                        setIsProfileDropdownOpen(!isProfileDropdownOpen)
-                      }
-                    >
-                      <span className="sr-only">Open user menu</span>
-                      <FontAwesomeIcon
-                        icon={faUser}
-                        className="w-5 h-5 text-white rounded-full"
-                      />
-                    </button>
-                  </div>
-                  <div
-                    className={`z-50 ${
-                      isProfileDropdownOpen ? "" : "hidden"
-                    } absolute right-0 top-full mt-2 my-4 text-base list-none bg-[#dcdcdd] text-[#2a2d2f] divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600`}
-                    id="dropdown-user"
+                <div className="relative">
+                  <button
+                    onClick={() =>
+                      setIsProfileDropdownOpen(!isProfileDropdownOpen)
+                    }
+                    className="flex items-center text-sm font-medium rounded-full focus:outline-none dark:text-gray-400"
                   >
-                    <div className="px-4 py-3" role="none">
-                      <p
-                        className="text-sm inline-flex items-center dark:text-white"
-                        role="none"
-                      >
-                        <FontAwesomeIcon
-                          icon={faUser}
-                          className="w-3 h-3 mx-1 p-1 bg-slate-200 rounded-full"
-                        />
-                        {user.username}
-                      </p>
+                    <span className="sr-only">Open user menu</span>
+                    <FontAwesomeIcon
+                      icon={faUser}
+                      className="w-6 h-6 text-gray-700 dark:text-gray-200"
+                    />
+                  </button>
+                  {isProfileDropdownOpen && (
+                    <div className="absolute right-0 z-50 mt-2 w-40 bg-white rounded-md shadow-lg dark:bg-gray-800">
+                      <ul>
+                        <li>
+                          <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600">
+                            Profile ({user.username})
+                          </p>
+                        </li>
+                        <li>
+                          <button
+                            onClick={handleLogout}
+                            className="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600"
+                          >
+                            Logout
+                          </button>
+                        </li>
+                      </ul>
                     </div>
-                    <ul className="py-1" role="none">
-                      {/* <li>
-                      <a
-                        href="/app"
-                        className="block px-4 py-2 text-[#4c5c68] text-sm  hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 hover:text-[#1985a1] dark:hover:text-white"
-                        role="menuitem"
-                      >
-                        dashboard
-                      </a>
-                    </li>*/}
-                    {/* {user.isPrime &&(<li>
-                      <a
-                        href="/app/manage"
-                        className="block px-4 py-2 text-[#4c5c68] text-sm  hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 hover:text-[#1985a1] dark:hover:text-white"
-                        role="menuitem"
-                      >
-                        Manage users
-                      </a>
-                    </li>)} */}
-                      <li>
-                        <a
-                          className="block cursor-pointer px-4 py-2 text-[#4c5c68] text-sm hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 hover:text-[#1985a1] dark:hover:text-white"
-                          role="menuitem"
-                          onClick={handleLogout}
-                        >
-                          Sign out
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
+                  )}
                 </div>
               )}
             </div>
@@ -170,67 +137,112 @@ function SideNav() {
 
       <aside
         id="logo-sidebar"
-        className={`fixed top-0 left-0 z-40 w-56 h-screen pt-28 transition-transform bg-[#dcdcdd] border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 
-            ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}
+        className={`fixed top-0 left-0 z-40 h-screen pt-28 transition-all duration-300 bg-[#dcdcdd] border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700 
+            ${
+              isMobile
+                ? `${
+                    isSidebarOpen ? "translate-x-0 w-48" : "-translate-x-full"
+                  }`
+                : `${isHovered ? "w-56" : "w-16"} translate-x-0`
+            }
           `}
         aria-label="Sidebar"
         ref={sidebarRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
-        <div className="h-full px-3 pb-4 overflow-y-auto bg-[#dcdcdd]  font-sans dark:bg-gray-800 dark:text-slate-50">
-          <ul className="space-y-2 font-bold ">
-            <li onClick={()=>setIsSidebarOpen(false)}>
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-[#dcdcdd] font-sans dark:bg-gray-800 dark:text-slate-50">
+          <ul className="space-y-2 font-bold">
+            <li onClick={() => isMobile && setIsSidebarOpen(false)}>
               <Link
                 to="/app"
-                className="flex items-center rounded-lg w-full p-2  hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
+                onClick={() => setIsSidebarOpen(false)}
+                className="flex items-center rounded-lg w-full p-2 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
               >
                 <FontAwesomeIcon
                   icon={faHome}
                   className="w-6 dark:text-gray-200 text-[#1985a1] h-6"
                 />
-                <span className="flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap">
+                <span
+                  className={`flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap transition-opacity duration-300 
+                  ${
+                    isMobile
+                      ? "opacity-100"
+                      : isHovered
+                      ? "opacity-100"
+                      : "opacity-0"
+                  }`}
+                >
                   Home
                 </span>
               </Link>
             </li>
-            <li onClick={()=>setIsSidebarOpen(false)}>
+            <li>
               <Link
                 to="/app/categories"
-                className="flex items-center rounded-lg w-full p-2  hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
+                onClick={() => setIsSidebarOpen(false)}
+                className="flex items-center rounded-lg w-full p-2 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
               >
                 <FontAwesomeIcon
                   icon={faTableCellsLarge}
                   className="w-6 dark:text-gray-200 text-[#1985a1] h-6"
                 />
-                <span className="flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap">
+                <span
+                  className={`flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap transition-opacity duration-300 ${
+                    isMobile
+                      ? "opacity-100"
+                      : isHovered
+                      ? "opacity-100"
+                      : "opacity-0"
+                  }`}
+                >
                   Categories
                 </span>
               </Link>
             </li>
-            <li onClick={()=>setIsSidebarOpen(false)}>
+            <li>
               <Link
                 to="/app/offers"
-                className="flex items-center rounded-lg w-full p-2  hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
+                onClick={() => setIsSidebarOpen(false)}
+                className="flex items-center rounded-lg w-full p-2 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
               >
                 <FontAwesomeIcon
                   icon={faSalesforce}
                   className="w-6 dark:text-gray-200 text-[#1985a1] h-6"
                 />
-                <span className="flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap">
+                <span
+                  className={`flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap transition-opacity duration-300 ${
+                    isMobile
+                      ? "opacity-100"
+                      : isHovered
+                      ? "opacity-100"
+                      : "opacity-0"
+                  }`}
+                >
                   Offers
                 </span>
               </Link>
             </li>
-            <li onClick={()=>setIsSidebarOpen(false)}>
+            <li>
               <Link
                 to="/app/about"
-                className="flex items-center rounded-lg w-full p-2  hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
+                onClick={() => setIsSidebarOpen(false)}
+                className="flex items-center rounded-lg w-full p-2 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 group"
               >
                 <FontAwesomeIcon
                   icon={faReadme}
                   className="w-6 dark:text-gray-200 text-[#1985a1] h-6"
                 />
-                <span className="flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap">
-                  About
+                <span
+                  className={`flex-1 dark:text-gray-200 ms-3 text-[#46494c] whitespace-nowrap transition-opacity duration-300 ${
+                    isMobile
+                      ? "opacity-100"
+                      : isHovered
+                      ? "opacity-100"
+                      : "opacity-0"
+                  }`}
+                >
+                  About Us
                 </span>
               </Link>
             </li>
